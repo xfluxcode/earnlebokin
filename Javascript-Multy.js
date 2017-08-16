d6a5e8312649e40603d8c4e870a773214937e17b
xfluxcode <xfluxcode.id@gmail.com>
2017-08-16 11:55:38 +0700

    Update Javascript-Multy.js

--- Javascript-Multy.js
+++ Javascript-Multy.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="how-alcohol-causes-mental-and-moral.html"}
 if(x=="2"){xxx="how-alcohol-retards-digestion.html"}
 if(x=="3"){xxx="medical-testimony-on-alcohol.html"}
 if(x=="4"){xxx="mental-disturbances-caused-by-alcohol.html"}
-if(x=="5"){xxx="how-alcohol-affects-brain.html"}a_to_ve[i].href="https://earnlebokin.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="how-alcohol-affects-brain.html"}a_to_ve[i].href="http://www.sosialmagz.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
